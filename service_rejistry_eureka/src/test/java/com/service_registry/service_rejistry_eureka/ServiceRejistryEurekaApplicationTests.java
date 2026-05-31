586800e33a9fd75c56747ecd201651efe3118d7a
ParasPalli <paraspalli1234@gmail.com>
2026-05-31 10:28:26 +0530

    Created Eureka Server

---
+++ service_rejistry_eureka/src/test/java/com/service_registry/service_rejistry_eureka/ServiceRejistryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service_registry.service_rejistry_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRejistryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
